9d7786feba216a3f388575a0326a4231cd8f1546
Zach Thomas <zthomas@accertify.com>
2018-04-02 15:02:00 -0500

    working rotation. upgraded obstacle to be a quadrilateral.

--- obstacle.js
+++ obstacle.js
@@ -1,23 +1,95 @@
 function Obstacle() {
-  this.position = createVector(width / 2, height / 2);
   this.height = 20;
   this.width = 100;
+  this.cornerTolerance = 8;
+  this.corners = [];
+
+  this.corners.push(createVector(width / 2 - 50, height / 2 - 10));
+  this.corners.push(createVector(width / 2 + 50, height / 2 - 10));
+  this.corners.push(createVector(width / 2 + 50, height / 2 + 10));
+  this.corners.push(createVector(width / 2 - 50, height / 2 + 10));
 
   this.collide = function(individual) {
-    if (individual.position.x > this.position.x - (.5 * this.width) &&
-      individual.position.x < this.position.x + (.5 * this.width) &&
-      individual.position.y > this.position.y - (0.5 * this.height) &&
-      individual.position.y < this.position.y + (0.5 * this.height)) {
-      return true;
-    } else {
-      return false;
+    if (!individual.crashed){
+      if (this.isInside(individual.position)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  this.isInside = function(otherPosition) {
+    // use top left corner of quadrilateral as origin, so it can be set to 0, 0
+    origin = createVector();
+    // downVector is the vector pointing from origin to the bottom left corner
+    downVector = p5.Vector.sub(this.corners[3], this.corners[0]);
+    // rightVector is the vector pointing from origin to the top right corner
+    rightVector = p5.Vector.sub(this.corners[1], this.corners[0]);
+    // pointVector is otherPosition translated into the space of the quadrilateral
+    pointVector = p5.Vector.sub(otherPosition, this.corners[0]);
+
+    if ((0 <= pointVector.dot(downVector) && pointVector.dot(downVector) <= downVector.dot(downVector)) &&
+        (0 <= pointVector.dot(rightVector) && pointVector.dot(rightVector) <= rightVector.dot(rightVector))) {
+          return true;
+        } else {
+          return false;
+        }
+  }
+
+  this.onCorner = function(otherPosition) {
+    for (var i = 0; i < this.corners.length; i++) {
+      if (abs(p5.Vector.dist(this.corners[i], otherPosition)) < this.cornerTolerance) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  this.center = function() {
+    c = createVector();
+
+    for (var i = 0; i < this.corners.length; i++) {
+      c.x += this.corners[i].x;
+      c.y += this.corners[i].y;
+    }
+
+    return c.div(this.corners.length);
+  }
+
+  this.rotate = function(angle) {
+    // 0 = top left
+    // 1 = top right
+    // 2 = bottom right
+    // 3 = bottom left
+    newCorners = [];
+    this.position = this.center();
+
+    for (var i = 0; i < 4; i++) {
+      newCorners[i] = createVector();
+      newCorners[i].x = this.position.x + (this.corners[i].x - this.position.x) * cos(angle) + (this.corners[i].y - this.position.y) * sin(angle)
+      newCorners[i].y = this.position.y - (this.corners[i].x - this.position.x) * sin(angle) + (this.corners[i].y - this.position.y) * cos(angle)
+    }
+
+    this.corners = newCorners;
+  }
+
+  this.translate = function(changeVector) {
+    for (var i = 0; i < this.corners.length; i++) {
+      this.corners[i].add(changeVector);
     }
   }
 
   this.show = function() {
+    push();
     noStroke();
     fill(255);
     rectMode(CENTER);
-    rect(this.position.x, this.position.y, this.width, this.height);
+    quad(this.corners[0].x, this.corners[0].y,
+         this.corners[1].x, this.corners[1].y,
+         this.corners[2].x, this.corners[2].y,
+         this.corners[3].x, this.corners[3].y);
+    pop();
   }
 }
--- sketch.js
+++ sketch.js
@@ -5,6 +5,9 @@ var target;
 var generationParagraph;
 var generationText = 0;
 var obstacle;
+var cooldown = 0;
+var grabbed = false;
+var cornerClicked = false;
 
 function setup() {
   createCanvas(600, 400);
@@ -18,25 +21,62 @@ function setup() {
   generationParagraph = createP("Generation: " + generationText);
 }
 
-function mouseClicked() {
-  if (mouseX < width && mouseX > 0 && mouseY < height && mouseY > 0) {
-    target.x = mouseX;
-    target.y = mouseY;
+function mouseDragged() {
+  cooldown = 20;
+  var mousePos = createVector(mouseX, mouseY)
+  if ((obstacle.isInside(mousePos) || grabbed) && !obstacle.onCorner(mousePos)) {
+    grabbed = true;
 
-    return false;
+    diffX = mouseX - pmouseX;
+    diffY = mouseY - pmouseY;
+
+    obstacle.translate(createVector(diffX, diffY))
   }
+  return false;
+}
+
+function mouseClicked() {
+  var mousePos = createVector(mouseX, mouseY)
+  if (mouseX < width && mouseX > 0 && mouseY < height && mouseY > 0 && cooldown == 0 &&
+      !cornerClicked &&
+      !obstacle.onCorner(mousePos)) {
+    if (!obstacle.isInside(mousePos)) {
+      target.x = mouseX;
+      target.y = mouseY;
+    }
+  } else if (obstacle.onCorner(mousePos)) {
+    grabbed = true;
+    cornerClicked = true;
+  } else if (mouseX < width && mouseX > 0 && mouseY < height && mouseY > 0 && cooldown == 0 && cornerClicked) {
+    cornerClicked = false;
+  }
+  return false;
+}
+
+function keyPressed() {
+  if (cornerClicked) {
+    if (keyCode === LEFT_ARROW) {
+      obstacle.rotate(PI / 32)
+    } else if (keyCode === RIGHT_ARROW) {
+      obstacle.rotate(-PI / 32)
+    } else if (keyCode === UP_ARROW) {
+      obstacle.rotate(PI / 8)
+    } else if (keyCode === DOWN_ARROW) {
+      obstacle.rotate(-PI / 8)
+    }
+  }
+
+  return false;
 }
 
 function draw() {
-  // target.x = mouseX
-  // target.y = mouseY
+  mousePos = createVector(mouseX, mouseY);
   background(0);
   obstacle.show();
   population.run();
   count++;
 
   ellipse(target.x, target.y, 16, 16);
-  // ellipse(mouseX, mouseY, 16, 16);
 
   if (count == lifespan) {
     population.evaluate();
@@ -44,5 +84,13 @@ function draw() {
     count = 0;
     generationText++;
     generationParagraph.html("Generation: " + generationText)
+  }
+
+  if (cooldown > 0) {
+    cooldown -= 1;
+  }
+
+  if (cooldown == 0) {
+    grabbed = false;
   }
 }
